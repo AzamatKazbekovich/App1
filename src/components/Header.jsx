c56d1e792f13bcfbc6758c978d4b7ed375f5550e
Azamat Sarsengaliev <abudiga@bk.ru>
2022-10-15 15:35:53 +0300

    13

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+
+const Header = () => {
+    return <header className='header'>
+        <img src='https://www.logodesignlove.com/wp-content/uploads/2022/01/logo-wave-symbol-01.jpg'/>
+      </header>
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,27 @@
+
+const Header = () => {
+    return <div className='content'>
+    <div>
+      <img src='https://images.ctfassets.net/hrltx12pl8hq/a2hkMAaruSQ8haQZ4rBL9/8ff4a6f289b9ca3f4e6474f29793a74a/nature-image-for-website.jpg?fit=fill&w=480&h=320'/>
+    </div>
+    <div>
+      ava + description
+    </div>
+    <div>
+      My posts
+      <div>
+        New post
+      </div>
+      <div>
+        <div>
+          Post 1
+        </div>
+        <div>
+          Post 2
+        </div>
+      </div>
+    </div>
+  </div>
+}
+
+export default Header;
