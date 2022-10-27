32e07cda41a43bb60aa250d799e9e9eefd55b4ba
Azamat Sarsengaliev <abudiga@bk.ru>
2022-10-27 16:55:31 +0300

    34

--- src/Redux/State.js
+++ src/Redux/State.js
@@ -1,3 +1,4 @@
+import {rerenderEntireTree} from '../render';
 
   let state = {
     profilePage: {
@@ -6,7 +7,8 @@
         {id:2, message: 'It is my first post', likesCount: 11},
         {id:3, message: 'It is', likesCount: 11},
         {id:4, message: 'It is my first', likesCount: 11}
-      ]
+      ],
+      newPostText: 'it camasutra'
     },
 
     dialogsPage: {
@@ -29,15 +31,24 @@
     sideBar: {}
   }
 
-  export let addPost = (postMessage) => {
+
+  export let addPost = () => {
   
     let newPost = {
     id: 5,
-    message: postMessage,
+    message: state.profilePage.newPostText,
     likesCount: 0
     };
     
     state.profilePage.posts.push(newPost);
+    state.profilePage.newPostText = '';
+    rerenderEntireTree(state);
+  }
+
+  export let updateNewPostText = (newText) => {
+  
+    state.profilePage.newPostText=newText;
+    rerenderEntireTree(state);
   }
 
   export default state;
